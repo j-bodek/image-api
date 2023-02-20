4571e279d9e67b1be736790df366c700cc5de050
Liljack118 <kamilzajaczek4@gmail.com>
2023-02-20 10:08:44 +0100

    create custom OverwriteStorage class

---
+++ api/src/core/storage.py
@@ -0,0 +1,18 @@
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import os
+
+
+class OverwriteStorage(FileSystemStorage):
+    """Overwrite FileSystemStorage class to overwrite
+    file with same path. This storage is used to save images thumbnails"""
+
+    def get_available_name(self, name):
+        """Returns a filename that's free on the target storage system, and
+        available for new content to be written to.
+        """
+
+        # If the filename already exists, remove it
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        return name
